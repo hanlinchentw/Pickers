cf0209d82c9a8010627cae88029cfaab3a0ac4e5
Han <s3876531@gmail.com>
2022-08-30 23:51:36 +0800

    feat: implement new mapView

---
+++ FoodPicker/MapView/MapView.swift
@@ -0,0 +1,22 @@
+//
+//  MapView.swift
+//  FoodPicker
+//
+//  Created by 陳翰霖 on 2022/8/30.
+//  Copyright © 2022 陳翰霖. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+  var body: some View {
+    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  }
+}
+
+struct MapView_Previews: PreviewProvider {
+  static var previews: some View {
+    MapView()
+  }
+}
