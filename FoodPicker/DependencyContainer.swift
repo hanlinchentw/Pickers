71622d81b00dd630865e3eff12f22ca59c357668
陳翰霖 <chenhanlin@Hans-MacBook-Pro.local>
2023-08-27 22:03:23 +0800

    feat: create DependencyContainer

---
+++ FoodPicker/DependencyContainer.swift
@@ -0,0 +1,15 @@
+//
+//  DependencyContainer.swift
+//  FoodPicker
+//
+//  Created by 陳翰霖 on 2023/8/27.
+//  Copyright © 2023 陳翰霖. All rights reserved.
+//
+
+import Foundation
+
+final class DependencyContainer {
+	func registerApiClient() {
+		
+	}
+}
